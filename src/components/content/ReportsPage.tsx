7a8a5b17bbc76098ab92e1a4131f943fedf53079
Eric Hedman <eric@ejhedman.com>
2025-07-02 14:34:10 -0400

    xrollers

--- src/components/content/ReportsPage.tsx
+++ src/components/content/ReportsPage.tsx
@@ -426,7 +426,7 @@ export function ReportsPage() {
           </Dialog>
         </div>
         
-        <div className="flex-1 overflow-y-auto p-4">
+        <div className="flex-1 overflow-y-auto min-h-0 p-4">
           {isLoading ? (
             <div className="text-center text-gray-500">Loading reports...</div>
           ) : (
